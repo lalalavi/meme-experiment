704d457c6bc864dd53f95bc7d9f3ff4490d2c3e9
Vi <violeta.cespedes@live.com>
2021-05-18 00:07:17 +0200

    vectorized images,between group allocation, history in 1st page

--- code.py
+++ code.py
@@ -12,7 +12,7 @@
 # SOME UNUSED CODE but was useful to learn about it
 
 
-# EmotionalStatus    = models.IntegerField(widget=widgets.RadioSelect, choices=[ [1, 'Quitebadyooo'], [2], [3], [4], [5], [6], [7, 'excellent'] ])
+# EmotionalStatus   = models.IntegerField(widget=widgets.RadioSelect, choices=[ [1, 'Quitebadyooo'], [2], [3], [4], [5], [6], [7, 'excellent'] ])
 # EmotionalStatus   = models.IntegerField(widget=widgets.RadioSelect, choices=[ [1, 'Strongly negative'], [2, 'Negative'], [3, 'Somewhat negative'], [4, 'Neutral'], [5, 'Somewhat positive'], [6, 'Positive'], [7, 'Strongly positive'] ])
 # Should emotional status be 5 or 7
 
@@ -24,6 +24,9 @@
 
 # sorted_string = json.dumps(x)
 
+# set vectors
+# let numbers = js_vars.sorted_string;
+# let vButtons = document.getElementsByClassName('lol').getAttribute("src");  
 
 # SOME STUFF FROM PAGE 1. TO MEME OR NOT TO MEME
 # 
@@ -34,8 +37,19 @@
 #     </button>
 #     <button type="button" onclick="sendClick(this)" value="See" id="See">
 #     </button>
-# </div>
+# </div
 
+#  {{ if past_players.iTrialDec == "Post" }} 
+        #     {{ include Constants.history_template }}
+        # {{ endif }}
+
+#  <div class="img-wrapper">
+                # <img id='lastp' src= {{ static Image }} style="width:100%"/>
+        
+
+#   <div><img id='??' src= {{ static Image }} /></div>
+
+# <td><div><img src= "{{ static Image }}" style="width:100%"></div></td>  !!!
 
 # <script>
 #     let trialId = null;
@@ -62,3 +76,66 @@
 #         liveSend({});
 #     });
 # </script> -->
+
+# <script>
+# 	// let white = js_vars.white;
+#     // let heart = js_vars.heart;
+
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":100,"y":295});
+# 	// emptyHeart=R.image("white").attr({"height":"90","width":"90","x":200,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":300,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":400,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":500,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":600,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":700,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":800,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":900,"y":295});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":1000,"y":295});
+
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":145,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":245,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":345,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":445,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":545,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":645,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":745,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":845,"y":425});
+# 	// emptyHeart=R.image(white).attr({"height":"90","width":"90","x":945,"y":425});
+
+
+# 	// if(iLikes>0){setTimeout( function(){var Like=R.image(heart).attr({fill: "#900", stroke: "#000", "stroke-width": 1, "height":"90","width":"90","x":100,"y":295});},400);} //Lay a Heart over the Meme if Liked
+# 	// if(iLikes>1){setTimeout( function(){var Like=R.image("heart").attr({"height":"90","width":"90","x":200,"y":295});},700);
+# 	// }
+# 	// if(iLikes>2){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":300,"y":295});},1000); 
+# 	// }if(iLikes>3){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":400,"y":295});},1300); 
+# 	// }
+# 	// if(iLikes>4){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":500,"y":295});},1600); 
+# 	// }
+# 	// if(iLikes>5){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":600,"y":295});},1900); 
+# 	// }
+# 	// if(iLikes>6){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":700,"y":295});},2200); 
+# 	// }
+# 	// if(iLikes>7){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":800,"y":295});},2500); 
+# 	// }
+# 	// if(iLikes>8){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":900,"y":295});},2800); 
+# 	// }
+# 	// if(iLikes>9){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":1000,"y":295});},3100); 
+# 	// }
+# 	// if(iLikes>10){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":145,"y":425});},3400); 
+# 	// }
+# 	// if(iLikes>11){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":245,"y":425});},3700); 
+# 	// }
+# 	// if(iLikes>12){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":345,"y":425});},4000); 
+# 	// }
+# 	// if(iLikes>13){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":445,"y":425});},4300); 
+# 	// }
+# 	// if(iLikes>14){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":545,"y":425});},4600); 
+# 	// }
+# 	// if(iLikes>15){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":645,"y":425});},4900); 
+# 	// }
+# 	// if(iLikes>16){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":745,"y":425});},5200); 
+# 	// }
+# 	// if(iLikes>17){setTimeout( function(){var Like=R.image(heart).attr({"height":"90","width":"90","x":845,"y":425});},5500); 
+# 	// }
+# 	// if(iLikes>18){setTimeout( function(){var Like=R.image(heart).attr({fill: "#900", stroke: "#000", "stroke-width": 1, "height":"90","width":"90","x":945,"y":425});},5800); 
+# 	// }
--- meme_game/__init__.py
+++ meme_game/__init__.py
@@ -1,6 +1,7 @@
 from otree.api import *
 import numpy as np
 from numpy import random
+import random
 from random import sample
 from random import choices
 import pandas as pd
@@ -22,7 +23,10 @@ class Constants(BaseConstants):
     players_per_group = None
     num_rounds = 10 #here you define the number of trials
     choices = ['Post', 'See'] 
+    history_template = 'meme_game/history.html'
     df = pd.read_excel("_static/global/HR.xlsx",index_col="Numbers") #does it matter that it is csv or xsls
+    df2 = pd.read_excel("_static/global/LR.xlsx",index_col="Numbers")
+
 
 class Subsession(BaseSubsession):
     pass
@@ -34,6 +38,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer): #define here ALL variables i will save at player level
 
+    treatment          = models.StringField(blank=True)
     sReward            = models.StringField(blank=True)
     iTrialDec          = models.StringField(choices=Constants.choices) #first decision where u can choose between seeing and posting
     iDec               = models.BooleanField(blank=True) #why is this a boolean
@@ -49,8 +54,8 @@ class Player(BasePlayer): #define here ALL variables i will save at player level
     sTag1              = models.StringField(blank=True)
     sTag2              = models.StringField(blank=True)
     sTag3              = models.StringField(blank=True)
-    iFeedbackLike      = models.IntegerField(blank=True)
-    iFeedbackDislike   = models.IntegerField(blank=True)
+    iLikes             = models.IntegerField(blank=True)
+    iDislikes          = models.IntegerField(blank=True)
     dRTDec1            = models.FloatField(blank=True)
     dRTFeed            = models.FloatField(blank=True) 
     dRTPost            = models.FloatField(blank=True) #d because double, reaction time
@@ -58,7 +63,7 @@ class Player(BasePlayer): #define here ALL variables i will save at player level
     dRTFeedback        = models.FloatField(blank=True)
     dRTEmotionalStatus = models.FloatField(blank=True) 
     sTreat             = models.StringField(blank=True)
-
+    last               = models.IntegerField(blank=True)
 
 
 
@@ -68,48 +73,60 @@ class Player(BasePlayer): #define here ALL variables i will save at player level
 
 def creating_session(subsession):
 
-    # randomize to treatments
+
     for player in subsession.get_players():
-        # 1. check round number (# Check variable names)
+        # randomize to treatments
+        if player.round_number == 1:
+            player.participant.treatment = random.choice(["Control", "Emotional"])
+        player.treatment = player.participant.treatment
+        print('set player.treatment to', player.treatment)
+
+        if player.round_number > 1:
+            prev_player = player.in_round(player.round_number - 1)
+
+         # 1. check round number (# Check variable names)
         if player.round_number > Constants.num_rounds/2:
             player.sReward = 'LR'
-            player.iImgPost1        = random.randint(low=101,high=len(os.listdir('_static/LR')))
-            player.iImgPost2        = random.randint(low=101,high=len(os.listdir('_static/LR')))
-            player.iImgPost3        = random.randint(low=101,high=len(os.listdir('_static/LR')))
-            player.iImgPost4        = random.randint(low=101,high=len(os.listdir('_static/LR')))
-            player.iImgPost5        = random.randint(low=101,high=len(os.listdir('_static/LR')))
-            player.iImgPost6        = random.randint(low=101,high=len(os.listdir('_static/LR')))
+            # dRange                  = range(101, len(os.listdir('_static/LR')))
+            # vImages                 = random.sample(range(dRange), 6)
+            vImages                 = random.sample(range(1,len(os.listdir('_static/HR'))), 6) #need to make this actually work
+            player.iImgPost1        = vImages[0]
+            player.iImgPost2        = vImages[1]
+            player.iImgPost3        = vImages[2]
+            player.iImgPost4        = vImages[3]
+            player.iImgPost5        = vImages[4]
+            player.iImgPost6        = vImages[5]
+            # player.iImgPost6      = random.randint(low=101,high=len(os.listdir('_static/LR')))
+             
         else:
             player.sReward = 'HR' 
-            player.iImgPost1        = random.randint(low=1,high=len(os.listdir('_static/HR')))
-            player.iImgPost2        = random.randint(low=1,high=len(os.listdir('_static/HR')))
-            player.iImgPost3        = random.randint(low=1,high=len(os.listdir('_static/HR')))
-            player.iImgPost4        = random.randint(low=1,high=len(os.listdir('_static/HR')))
-            player.iImgPost5        = random.randint(low=1,high=len(os.listdir('_static/HR')))
-            player.iImgPost6        = random.randint(low=1,high=len(os.listdir('_static/HR')))
+            # dRange                  = range(1,len(os.listdir('_static/HR')))
+            # vImages                 = random.sample(range(dRange), 6)
+            vImages                 = random.sample(range(1,len(os.listdir('_static/HR'))), 6)
+            player.iImgPost1        = vImages[0]
+            player.iImgPost2        = vImages[1]
+            player.iImgPost3        = vImages[2]
+            player.iImgPost4        = vImages[3]
+            player.iImgPost5        = vImages[4]
+            player.iImgPost6        = vImages[5]
+            
+        # 2.  # depending on reward we check different images,
+        # then we sample six images from respective folder and...
+        # 3. save image post in the variable
 
         print('set player.sReward to', player.sReward)
-        # depending on reward we check different images
-        # 2. sample six  images from respective folder
 
-        # 3. save image post in the variable
         # Setup a treatment condition (changes each round)
         player.sTreat          = random.choice(['Like', 'Dislike'])
         print('set player.sTreat to', player.sTreat)
 
-        player.iImgFeed         = random.randint(low=1,high=len(os.listdir('_static/feed_memes')))  
-        #!!!!!!! subsamples
-        
-        # create 
+        player.iImgFeed         = random.randint(1,len(os.listdir('_static/feed_memes')))  
+        #!!!!!!! subsamples ?
+
         print('set player.iImgFeed to', player.iImgFeed)
         print('set player.iImgPost1 to', player.iImgPost1)
 
-    # # decide what type of feedback player will see depending on treatment
-    # for participant in participant.treatment:
-    #     if partcipant.treatment == '1' #in emotional treatment
-    #         # print('lalala') display feedback with likes and dislikes
-    
-
+# def past_players=player.in_previous_rounds()
 
 # PAGES
 
@@ -117,8 +134,35 @@ class ToMemeOrNotToMeme(Page):
     form_model = 'player'
     form_fields = [
         'iTrialDec',
-        'dRTDec1'
+        'dRTDec1',
     ]
+    
+    @staticmethod
+    def vars_for_template(player): 
+        if player.round_number > 1:
+            return dict(prev_player = player.in_round(player.round_number - 1)) #define the last round
+            if prev_player.iTrialDec == 'Post':
+                return {
+                    'lala'    :  "".join([player.in_round(player.round_number - 1).sReward,'/meme', str(player.in_round(player.round_number - 1).iDec2) , '.jpg']),
+                    # 'prev_player' : player.in_round(player.round_number - 1)
+                }
+
+class history(Page):
+    form_model = 'player' #from who are you extracting the info
+
+    @staticmethod
+    def vars_for_template(player): 
+        return {
+            'Image'    :  "".join([prev_player.sReward,'/meme', str(prev_player.iDec2) , '.jpg']) ,
+        }
+    
+    # @staticmethod
+    # def vars_for_template(player: Player):
+    #     return dict(past_players=player.in_previous_rounds())
+    
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number > 1
 
 class Feed(Page):
     form_model = 'player' #from who are you extracting the info
@@ -187,7 +231,7 @@ class Posting(Page):
     @staticmethod
     def js_vars(player: Player):
         return {
-            'ImageID'    :  player.iImgPost1 ,
+            'ImageID'     :  player.iImgPost1 ,
             'Image2ID'    :  player.iImgPost2 ,
             'Image3ID'    :  player.iImgPost3 ,
             'Image4ID'    :  player.iImgPost4 ,
@@ -220,29 +264,34 @@ class ResultsWaitPage(WaitPage):
 class Feedback(Page):
     form_model = 'player'
     form_fields = [
-        'iFeedbackLike',
-        'iFeedbackDislike',
+        'iLikes',
+        'iDislikes',
         'dRTFeedback'
     ] 
 
     @staticmethod
     def vars_for_template(player): 
         return {
-            'img'               : player.iDec2,
-            'likes'             : Constants.df.loc[player.iDec2,"Likes"],
-            'dislikes'          : Constants.df.loc[player.iDec2,"Dislikes"]
+            'img'        : player.iDec2,
+            'l'          : Constants.df.loc[player.iDec2,"Likes"],
+            'd'          : Constants.df.loc[player.iDec2,"Dislikes"]
         }
 
     @staticmethod
     def is_displayed(player):
         return player.iTrialDec == 'Post'
 
+    @staticmethod
+    def js_vars(player: Player):
+        return {
+            'treatment'   :  player.treatment ,
+        }
+
+
         
 #! THINGS TO  BE CODED 
-# Feedbackpage (!!!!) major problem
 # ToMemeOrNotToMeme: better layout
-# History display
 # QUESTIONNAIRE APP with social media and demographics question
 # ResultsWaitPage
 
-page_sequence = [ToMemeOrNotToMeme, Feed, Posting, HowDoYaFeel, addTags, Feedback]
+page_sequence = [ToMemeOrNotToMeme, Feed, Posting, addTags, Feedback, HowDoYaFeel]
